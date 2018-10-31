7e278cc07020e9f63f7fac4c76444f6ec397fdae
Dan Hadarau <dhadarau@untangle.com>
2018-10-31 15:26:11 +0200

    MFW packages startup

---
+++ package/settings/Main.js
@@ -0,0 +1,12 @@
+Ext.define('Mfw.settings.Main', {
+    extend: 'Ext.Panel',
+    alias: 'widget.settings-main',
+
+    layout: 'fit',
+
+    items: [{
+        xtype: 'component',
+        html: 'main setting'
+    }]
+
+});
