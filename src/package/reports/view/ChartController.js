489413facdc3fbb1081d9b843d5d11c6da0d0a04
Dan Hadarau <dhadarau@untangle.com>
2019-01-15 18:08:23 +0200

    Sort SERIES tooltip items by value descending
    fix for PIE charts

--- src/package/reports/view/ChartController.js
+++ src/package/reports/view/ChartController.js
@@ -127,6 +127,19 @@ Ext.define('Mfw.reports.ChartController', {
                 style: {
                     // fontSize: isWidget ? '12px' : '14px'
                 },
+                formatter: function (tooltip) {
+                    var items = this.points;
+
+                    // sort the values
+                    if (items) {
+                        items.sort(function(a, b) {
+                            return ((a.y < b.y) ? -1 : ((a.y > b.y) ? 1 : 0));
+                        });
+                        items.reverse();
+                    }
+
+                    return tooltip.defaultFormatter.call(this, tooltip);
+                },
                 headerFormat: '<p style="margin: 0 0 5px 0; color: #555;">{point.key}</p>'
             },
             legend: {
@@ -292,19 +305,6 @@ Ext.define('Mfw.reports.ChartController', {
                 },
                 colors: colors,
                 plotOptions: plotOptions,
-                tooltip: {
-                    formatter: function (tooltip) {
-                        var items = this.points;
-
-                        // sort the values
-                        items.sort(function(a, b) {
-                            return ((a.y < b.y) ? -1 : ((a.y > b.y) ? 1 : 0));
-                        });
-                        items.reverse();
-
-                        return tooltip.defaultFormatter.call(this, tooltip);
-                    }
-                },
                 legend: {
                     enabled: !isWidget
                 },
