2bdd1c5c91cf3592f2118455bd32bd0bea9b5542
Dan Hadarau <dhadarau@untangle.com>
2019-02-27 16:11:23 +0200

    Guess timezone in setup wizard instead of setting UTC
    MFW-283

--- src/app/setup/step/Step2Account.js
+++ src/app/setup/step/Step2Account.js
@@ -82,6 +82,7 @@ Ext.define('Mfw.setup.step.Account', {
                 labelTextAlign: 'right',
                 required: true,
                 valueField: 'text',
+                value: moment.tz.guess(),
                 options: Globals.timezones
             }]
         }]
@@ -106,32 +107,8 @@ Ext.define('Mfw.setup.step.Account', {
         },
 
         onActivate: function (view) {
-            var me = this;
             view.down('formpanel').reset(true);
-            me.loadTimezone();
         },
-
-        loadTimezone: function () {
-            var me = this,
-                form = me.getView().down('formpanel');
-
-            Ext.Ajax.request({
-                url: '/api/settings/system/timeZone',
-                success: function (result) {
-                    var tz = Ext.decode(result.responseText);
-                    if (!tz || tz === null) {
-                        form.getFields('displayName').setValue('UTC');
-                    } else {
-                        form.getFields('displayName').setValue(tz.displayName || 'UTC');
-                    }
-
-                },
-                failure: function () {
-                    console.warn('Unable to load Timezone!');
-                }
-            });
-        },
-
 
         setAccount: function () {
             var me = this,
