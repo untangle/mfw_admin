05d80c72bba4c8a630f9d8c9bb9229e6ba333023
Dan Hadarau <dhadarau@untangle.com>
2019-03-11 16:17:43 +0200

    Updated the Login view to a more flat UI

--- src/common/auth/Auth.js
+++ src/common/auth/Auth.js
@@ -2,23 +2,34 @@ Ext.define('Mfw.Auth', {
     extend: 'Ext.Container',
     alias: 'widget.auth',
 
-    style: 'background: #999',
-
     config: {
         redirectTo: null
     },
 
-    layout: 'center',
+    layout: {
+        type: 'vbox',
+        align: 'center'
+    },
 
     padding: '0 0 200 0',
 
     items: [{
-        xtype: 'panel',
-        shadow: true,
-        width: 320,
-        height: 'auto',
-        bodyPadding: 16,
+        xtype: 'component',
+        docked: 'top',
+        margin: '24 0 0 0',
+        style: 'text-align: center;',
+        html: '<img src="/static/res/untangle-logo.png" style="vertical-align: middle; margin-right: 16px;"/>'
+    }, {
+        xtype: 'component',
+        width: 500,
+        padding: '0 0 24 0',
+        html: '<h1 style="text-align: center; font-weight: 100; font-size: 24px; color: #333;">Log In</h1><hr/>'
+    }, {
+        xtype: 'formpanel',
+        scrollable: false,
+        padding: 0,
 
+        width: 300,
         keyMapEnabled: true,
         keyMap: {
             enter: {
@@ -26,65 +37,50 @@ Ext.define('Mfw.Auth', {
                 handler: 'onLogin'
             }
         },
-
-        style: 'border-radius: 8px;',
-
+        layout: {
+            type: 'vbox',
+            align: 'center'
+        },
+        defaults: {
+            clearable: false,
+            labelAlign: 'top',
+            width: 250,
+            autoComplete: false,
+            errorTarget: 'side',
+            userCls: 'x-custom-field',
+            animateUnderline: false,
+            required: true
+        },
         items: [{
-            xtype: 'toolbar',
-            docked: 'top',
-            style: 'background: none; font-size: 24px; font-weight: 100; color: #777;',
-            shadow: false,
-            padding: '16 16 0 16',
-            items: [{
-                xtype: 'component',
-                html: '<img src="/static/res/untangle-logo.png" width=90 style="vertical-align: middle; margin-right: 16px;"/>'
-            }, '->', {
-                xtype: 'component',
-                html: 'Login'
-            }]
+            xtype: 'component',
+            itemId: 'error',
+            hidden: true,
+            html: '<p style="color: red; font-size: 12px; text-align: center;"><i class="x-fa fa-exclamation-triangle"></i> Invalid username or password!</p>'
         }, {
-            xtype: 'formpanel',
-            bodyPadding: 0,
-            layout: 'vbox',
-            keyMapEnabled: true,
-            keyMap: {
-                enter: {
-                    key: Ext.event.Event.ENTER,
-                    handler: 'onLogin'
-                }
-            },
-            defaults: {
-                autoComplete: false,
-                labelAlign: 'left',
-                labelTextAlign: 'right'
-            },
-            items: [{
-                xtype: 'component',
-                itemId: 'error',
-                hidden: true,
-                html: '<p style="color: red; font-size: 12px; text-align: center;"><i class="x-fa fa-exclamation-triangle"></i> Invalid username or password!</p>'
-            }, {
-                xtype: 'textfield',
-                name: 'username',
-                label: 'Username',
-                clearable: false,
-                required: true,
-                listeners: {
-                    painted: function (f) { f.focus(); }
-                }
-            }, {
-                xtype: 'passwordfield',
-                name: 'password',
-                label: 'Password',
-                clearable: false,
-                required: true
-            }, {
-                xtype: 'button',
-                text: 'Log In',
-                ui: 'action',
-                margin: '48 0 0 0',
-                handler: 'onLogin'
-            }]
+            xtype: 'textfield',
+            name: 'username',
+            label: 'Username',
+            listeners: {
+                painted: function (f) { f.focus(); }
+            }
+        }, {
+            xtype: 'passwordfield',
+            name: 'password',
+            label: 'Password',
+        }, {
+            xtype: 'component',
+            itemId: 'pending',
+            hidden: true,
+            style: 'text-align: center',
+            margin: '32 0 0 0',
+            html: '<i class="fa fa-spinner fa-spin fa-2x fa-fw"></i>'
+        }, {
+            xtype: 'button',
+            margin: '32 0 0 0',
+            width: 120,
+            text: 'Sign In',
+            ui: 'action',
+            handler: 'onLogin'
         }]
     }],
 
@@ -96,11 +92,14 @@ Ext.define('Mfw.Auth', {
         onLogin: function () {
             var me = this,
                 form = me.getView().down('formpanel'),
-                btn = form.down('button');
+                btn = form.down('button'),
+                pending = form.down('#pending');
 
             if (!form.validate()) { return; }
 
-            btn.setDisabled(true);
+            btn.hide();
+            pending.show();
+
             Ext.Ajax.request({
                 url: '/account/login',
                 method: 'POST',
@@ -111,7 +110,8 @@ Ext.define('Mfw.Auth', {
                 },
                 failure: function () {
                     form.down('#error').show();
-                    btn.setDisabled(false);
+                    btn.show();
+                    pending.hide();
                 }
             });
         },
