42b86b4f1a2f450268e223e6f39411dc84a25c29
Dan Hadarau <dhadarau@untangle.com>
2019-03-20 12:32:37 +0200

    Sync tables fields with backend

--- src/common/Table.js
+++ src/common/Table.js
@@ -21,6 +21,10 @@ Ext.define('Mfw.Table', {
             dataIndex: 'end_time',
             hidden: true
         }, {
+            text: 'Family',
+            dataIndex: 'family',
+            hidden: true
+        }, {
             text: 'IP Protocol',
             dataIndex: 'ip_protocol',
             width: 150,
@@ -124,12 +128,6 @@ Ext.define('Mfw.Table', {
             text: 'Server Longitude',
             dataIndex: 'server_longitude'
         }, {
-            text: 'C2S bytes',
-            dataIndex: 'c2s_bytes'
-        }, {
-            text: 'S2C bytes',
-            dataIndex: 's2c_bytes'
-        }, {
             text: 'Application ID',
             dataIndex: 'application_id'
         }, {
@@ -173,6 +171,106 @@ Ext.define('Mfw.Table', {
             text: 'Server DNS Hint',
             dataIndex: 'server_dns_hint'
         }],
+    },
+
+    session_stats: {
+        columns: [{
+            text: 'Session Id',
+            dataIndex: 'session_id'
+        }, {
+            text: 'Time Stamp',
+            dataIndex: 'time_stamp'
+        }, {
+            text: 'Bytes',
+            dataIndex: 'bytes'
+        }, {
+            text: 'Client Bytes',
+            dataIndex: 'client_bytes'
+        }, {
+            text: 'Server Bytes',
+            dataIndex: 'server_bytes'
+        }, {
+            text: 'Byte Rate',
+            dataIndex: 'byte_rate'
+        }, {
+            text: 'Client Byte Rate',
+            dataIndex: 'client_byte_rate'
+        }, {
+            text: 'Server Byte Rate',
+            dataIndex: 'server_byte_rate'
+        }, {
+            text: 'Packets',
+            dataIndex: 'packets'
+        }, {
+            text: 'Client Packets',
+            dataIndex: 'client_packets'
+        }, {
+            text: 'Server Packets',
+            dataIndex: 'server_packets'
+        }, {
+            text: 'Packet Rate',
+            dataIndex: 'packet_rate'
+        }, {
+            text: 'Client Packet Rate',
+            dataIndex: 'client_packet_rate'
+        }, {
+            text: 'Server Packet Rate',
+            dataIndex: 'server_packet_rate'
+        }]
+    },
+
+    interface_stats: {
+        columns: [
+            { text: 'Time Stamp', dataIndex: 'time_stamp' },
+            { text: 'Interface Id', dataIndex: 'interface_id' },
+            { text: 'Device Name', dataIndex: 'device_name' },
+            { text: 'Latency 1', dataIndex: 'latency_1' },
+            { text: 'Latency 5', dataIndex: 'latency_5' },
+            { text: 'Latency 15', dataIndex: 'latency_15' },
+            { text: 'Latency Variance', dataIndex: 'latency_variance' },
+            { text: 'Passive Latency 1', dataIndex: 'passive_latency_1' },
+            { text: 'Passive Latency 5', dataIndex: 'passive_latency_5' },
+            { text: 'Passive Latency 15', dataIndex: 'passive_latency_15' },
+            { text: 'Passive Latency Variance', dataIndex: 'passive_latency_variance' },
+            { text: 'Active Latency 1', dataIndex: 'active_latency_1' },
+            { text: 'Active Latency 5', dataIndex: 'active_latency_5' },
+            { text: 'Active Latency 15', dataIndex: 'active_latency_15' },
+            { text: 'Active Latency Variance', dataIndex: 'active_latency_variance' },
+            { text: 'Ping Timeout', dataIndex: 'ping_timeout' },
+            { text: 'Ping Timeout Rate', dataIndex: 'ping_timeout_rate' },
+            { text: 'RX Bytes', dataIndex: 'rx_bytes' },
+            { text: 'RX Bytes Rate', dataIndex: 'rx_bytes_rate' },
+            { text: 'RX Packets', dataIndex: 'rx_packets' },
+            { text: 'RX Packets Rate', dataIndex: 'rx_packets_rate' },
+            { text: 'RX Errors', dataIndex: 'rx_errs' },
+            { text: 'RX Errors Rate', dataIndex: 'rx_errs_rate' },
+            { text: 'RX Drop', dataIndex: 'rx_drop' },
+            { text: 'RX Drop Rate', dataIndex: 'rx_drop_rate' },
+            { text: 'RX Fifo', dataIndex: 'rx_fifo' },
+            { text: 'RX Fifo Rate', dataIndex: 'rx_fifo_rate' },
+            { text: 'RX Frame', dataIndex: 'rx_frame' },
+            { text: 'RX Frame Rate', dataIndex: 'rx_frame_rate' },
+            { text: 'RX Compressed', dataIndex: 'rx_compressed' },
+            { text: 'RX Compressed Rate', dataIndex: 'rx_compressed_rate' },
+            { text: 'RX Multicast', dataIndex: 'rx_multicast' },
+            { text: 'RX Multicast Rate', dataIndex: 'rx_multicast_rate' },
+            { text: 'TX Bytes', dataIndex: 'tx_bytes' },
+            { text: 'TX Bytes Rate', dataIndex: 'tx_bytes_rate' },
+            { text: 'TX Packets', dataIndex: 'tx_packets' },
+            { text: 'TX Packets Rate', dataIndex: 'tx_packets_rate' },
+            { text: 'TX Errors', dataIndex: 'tx_errs' },
+            { text: 'TX Errors Rate', dataIndex: 'tx_errs_rate' },
+            { text: 'TX Drop', dataIndex: 'tx_drop' },
+            { text: 'TX Drop Rate', dataIndex: 'tx_drop_rate' },
+            { text: 'TX Fifo', dataIndex: 'tx_fifo' },
+            { text: 'TX Fifo Rate', dataIndex: 'tx_fifo_rate' },
+            { text: 'TX Colls', dataIndex: 'tx_colls' },
+            { text: 'TX Colls Rate', dataIndex: 'tx_colls_rate' },
+            { text: 'TX Carrier', dataIndex: 'tx_carrier' },
+            { text: 'TX Carrier Rate', dataIndex: 'tx_carrier_rate' },
+            { text: 'TX Compressed', dataIndex: 'tx_compressed' },
+            { text: 'TX Compressed Rate', dataIndex: 'tx_compressed_rate' }
+        ]
     },
 
 
