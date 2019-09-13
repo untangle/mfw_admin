14883562344f2bcc336b4e9b200dfc5125634a9f
mahotz <mahotz@untangle.com>
2019-09-13 11:20:26 -0700

    Added default AUTO for wirelessMode

--- src/package/settings/model/Interface.js
+++ src/package/settings/model/Interface.js
@@ -154,7 +154,7 @@ Ext.define('Mfw.model.Interface', {
         { name: 'wirelessMode',       type: 'string', allowNull: true }, // ["AP", "CLIENT"]
         { name: 'wirelessPassword',   type: 'string', allowNull: true },
         { name: 'wirelessChannel',    type: 'integer', allowNull: true },
-        { name: 'wirelessMode',       type: 'string', allowNull: true },
+        { name: 'wirelessMode',       type: 'string', allowNull: false, defaultValue: 'AUTO' },
 
         // OpenVPN
         { name: 'openvpnUsernamePasswordEnabled', type: 'boolean', allowNull: true, defaultValue: false },
