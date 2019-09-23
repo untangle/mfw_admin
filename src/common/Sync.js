85f938d1fa2c86911f4b6eebb4ed968c7d196912
Dan Hadarau <dhadarau@untangle.com>
2019-09-23 11:57:18 +0300

    Redirect to Login view when sessison expires
    A message box alers user in such case

--- src/common/Sync.js
+++ src/common/Sync.js
@@ -236,6 +236,38 @@ Ext.define('Mfw.Sync', {
     exception: function (response, title) {
         var exception, summary, stack, isSync = false;
 
+        // auth fail
+        if (response.status === 403) {
+            // avoid displaying the exception bottom sheet
+            this.sheet.hide();
+
+            /**
+             * display auth fail message box
+             * prevent showing more than a single dialog in case of multiple calls exceptions
+             */
+            if (!this.authExceptionDialog) {
+                this.authExceptionDialog = Ext.create('Ext.MessageBox', {
+                    title: 'Authentication failed',
+                    message: 'Session has expired. Please login.',
+                    width: 300,
+                    showAnimation: null,
+                    hideAnimation: null,
+                    buttons: [{
+                        text: 'OK',
+                        ui: 'action',
+                        handler: function (btn) {
+                            // reloading document will redirect to auth
+                            document.location.reload();
+                        }
+                    }]
+                }).show();
+                this.authExceptionDialog.show();
+            }
+            return;
+        }
+
+
+
         // if it's a sync API call
         if (response.request.url.includes('/api/settings')) {
             isSync = true;
