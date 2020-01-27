cd598661f1c99ac7005016f1b36b58879f1cc7c4
Dan Hadarau <dhadarau@untangle.com>
2020-01-27 14:18:41 +0200

    Updated wireguard interface UI

--- src/package/settings/view/network/interface/Wireguard.js
+++ src/package/settings/view/network/interface/Wireguard.js
@@ -28,15 +28,17 @@ Ext.define('Mfw.settings.interface.Wireguard', {
             text: 'Peers',
             card: 'wg-peers',
             bind: {
-                ui: '{activeCard === "wg-peers" ? "action" : ""}'
+                ui: '{activeCard === "wg-peers" || activeCard === "wg-peer" ? "action" : ""}'
             },
             handler: 'setActiveCard'
         }]
     }, {
-        xtype: 'panel',
+        xtype: 'container',
         flex: 1,
-        itemId: 'card-panel',
-        layout: 'card',
+        layout: {
+            type: 'card',
+            animation: 'slide'
+        },
         bind: {
             activeItem: '#{activeCard}',
             // hidden: '{!intf}'
@@ -50,6 +52,49 @@ Ext.define('Mfw.settings.interface.Wireguard', {
                 flex: '{isDialog ? 1 : "auto"}'
             },
             items: [{
+                xtype: 'component',
+                docked: 'top',
+                padding: 16,
+                style: 'font-weight: 100; font-size: 20px;',
+                html: 'Main settings'
+            }, {
+                xtype: 'containerfield',
+                layout: {
+                    type: 'hbox',
+                },
+                items: [{
+                    xtype: 'textfield',
+                    name: 'wireguardAddress',
+                    label: 'Address',
+                    placeholder: 'enter address ...',
+                    clearable: false,
+                    autoComplete: false,
+                    labelAlign: 'top',
+                    flex: 1,
+                    bind: {
+                        value: '{intf.wireguardAddresses}',
+                        required: '{intf.type === "WIREGUARD"}',
+                        disabled: '{!intf.type === "WIREGUARD"}'
+                    },
+                    validators: 'ipv4'
+                }, {
+                    xtype: 'numberfield',
+                    name: 'wireguardPort',
+                    width: 100,
+                    margin: '0 0 0 32',
+                    label: 'Port',
+                    labelAlign: 'top',
+                    clearable: false,
+                    autoComplete: false,
+                    placeholder: 'enter port ...',
+                    bind: {
+                        value: '{intf.wireguardPort}',
+                        required: '{intf.type === "WIREGUARD"}',
+                        disabled: '{!intf.type === "WIREGUARD"}'
+                    },
+                    validators: 'port',
+                }]
+            }, {
                 xtype: 'containerfield',
                 layout: {
                     type: 'hbox',
@@ -61,6 +106,8 @@ Ext.define('Mfw.settings.interface.Wireguard', {
                     label: 'Private Key',
                     labelAlign: 'top',
                     editable: false,
+                    clearable: false,
+                    focusable: false,
                     placeholder: 'generate key ...',
                     flex: 1,
                     bind: {
@@ -70,10 +117,86 @@ Ext.define('Mfw.settings.interface.Wireguard', {
                     }
                 }, {
                     xtype: 'button',
-                    text: 'Generate',
-                    ui: 'action',
-                    margin: '0 0 0 16'
+                    iconCls: 'x-fa fa-sync',
+                    ui: 'round',
+                    margin: '0 0 0 16',
+                    handler: 'generatePrivateKey'
                 }]
+            }],
+            listeners: {
+                painted: 'clearErrors'
+            }
+        }, {
+            xtype: 'container',
+            itemId: 'wg-peers',
+            layout: 'fit',
+            items: [{
+                xtype: 'container',
+                docked: 'top',
+                layout: {
+                    type: 'hbox',
+                    align: 'middle'
+                },
+                padding: '16',
+                items: [{
+                    xtype: 'component',
+                    flex: 1,
+                    style: 'font-weight: 100; font-size: 20px;',
+                    html: 'Add or remove Peers'
+                }, {
+                    xtype: 'button',
+                    iconCls: 'x-fa fa-plus',
+                    ui: 'round action',
+                    handler: 'showPeerCard'
+                }]
+            }, {
+                xtype: 'grid',
+                userCls: 'c-noheaders',
+                flex: 1,
+                bind: '{intf.wireguardPeers}',
+                hideHeaders: true,
+                selectable: false,
+                columns: [{
+                    dataIndex: 'publicKey',
+                    flex: 1,
+                    sortable: false,
+                    hideable: false,
+                    resizable: false,
+                    menuDisabled: true,
+                    cell: {
+                        encodeHtml: false
+                    },
+                    renderer: function (val, rec) {
+                        return '<b>Public key:</b><br/>' + rec.get('publicKey') + '<br/>' +
+                               '<b>Allowed IPs:</b><br/> ' + rec.get('allowedIps')
+                    }
+                }, {
+                    align: 'center',
+                    width: 40,
+                    sortable: false,
+                    hideable: false,
+                    resizable: false,
+                    menuDisabled: true,
+                    cell: {
+                        tools: {
+                            delete: {
+                                iconCls: 'md-icon-delete',
+                                align: 'right',
+                                handler: 'onRemovePeer'
+                            }
+                        }
+                    }
+                }]
+            }]
+        }, {
+            xtype: 'formpanel',
+            itemId: 'wg-peer',
+            layout: 'vbox',
+            items: [{
+                xtype: 'component',
+                padding: '16 0',
+                style: 'font-weight: 100; font-size: 20px;',
+                html: 'Add Peer'
             }, {
                 xtype: 'containerfield',
                 layout: {
@@ -82,59 +205,59 @@ Ext.define('Mfw.settings.interface.Wireguard', {
                 },
                 items: [{
                     xtype: 'textfield',
-                    name: 'wireguardPublicKey',
+                    name: 'publicKey',
                     label: 'Public Key',
                     editable: false,
                     placeholder: 'generate key ...',
                     clearable: false,
+                    focusable: false,
                     labelAlign: 'top',
                     flex: 1,
                     bind: {
-                        value: '{intf.wireguardPublicKey}', // not in schema
-                        required: '{intf.type === "WIREGUARD"}',
-                        disabled: '{intf.type !== "WIREGUARD"}'
+                        required: '{activeCard === "wg-peer"}'
                     }
                 }, {
                     xtype: 'button',
-                    text: 'Generate',
-                    ui: 'action',
-                    margin: '0 0 0 16'
+                    iconCls: 'x-fa fa-sync',
+                    ui: 'round',
+                    margin: '0 0 0 16',
+                    handler: 'generatePublicKey'
                 }]
             }, {
-                xtype: 'textfield',
-                name: 'wireguardAddresses',
-                label: 'Addresses',
-                placeholder: 'enter comma separated IPs ...',
+                xtype: 'textareafield',
+                name: 'allowedIps',
+                label: 'Allowed IPs (comma separated values)',
+                maxRows: 2,
+                placeholder: 'e.g. 1.2.3.4/32',
                 clearable: false,
-                autoComplete: false,
                 labelAlign: 'top',
-                flex: 1,
                 bind: {
-                    value: '{intf.wireguardAddresses}',
-                    required: '{intf.type === "WIREGUARD"}',
-                    disabled: '{!intf.type === "WIREGUARD"}'
-                },
-                validators: 'ipv4'
+                    required: '{activeCard === "wg-peer"}'
+                }
             }, {
-                xtype: 'numberfield',
-                name: 'wireguardPort',
-                width: 100,
-                label: 'Port',
-                labelAlign: 'top',
-                placeholder: 'enter port ...',
-                bind: {
-                    value: '{intf.wireguardPort}',
-                    required: '{intf.type === "WIREGUARD"}',
-                    disabled: '{!intf.type === "WIREGUARD"}'
+                xtype: 'container',
+                layout: {
+                    type: 'hbox'
                 },
-                validators: 'port',
+                margin: '16 0',
+                items: [{
+                    xtype: 'component',
+                    flex: 1
+                }, {
+                    xtype: 'button',
+                    margin: '0 16 0 0',
+                    text: 'Cancel',
+                    handler: 'onCancelPeer'
+                }, {
+                    xtype: 'button',
+                    text: 'Add Peer',
+                    ui: 'action',
+                    handler: 'onAddPeer'
+                }]
             }],
             listeners: {
                 painted: 'clearErrors'
             }
-        }, {
-            xtype: 'grid',
-            itemId: 'wg-peers'
         }]
     }],
 
@@ -148,6 +271,63 @@ Ext.define('Mfw.settings.interface.Wireguard', {
 
         setActiveCard: function (btn) {
             this.getViewModel().set('activeCard', btn.card);
+        },
+
+        showPeerCard: function () {
+            var me = this;
+            me.getViewModel().set('activeCard', 'wg-peer');
+        },
+
+        onCancelPeer: function (btn) {
+            var me = this, form = btn.up('formpanel');
+            form.reset(true);
+            me.getViewModel().set('activeCard', 'wg-peers');
+        },
+
+        onAddPeer: function (btn) {
+            var me = this, form = btn.up('formpanel')
+            vm = me.getViewModel(), intf = vm.get('intf');
+
+            if (!form.validate()) {
+                Ext.toast('Please fill or correct invalid fields!', 3000);
+                return;
+            }
+            intf.wireguardPeers().add(form.getValues());
+            form.reset(true);
+            me.getViewModel().set('activeCard', 'wg-peers');
+        },
+
+        onRemovePeer: function (grid, info) {
+            info.record.drop();
+        },
+
+
+        generatePrivateKey: function () {
+            var me = this,
+            vm = me.getViewModel();
+
+            /**
+             * just for demo generate a key
+             * needs to be generated on the backend using wg genkey
+             */
+            var key = btoa(Math.random().toFixed(32).substr(2));
+
+            vm.get('intf').set('wireguardPrivateKey', key);
+
+        },
+
+        /**
+         * generates the peer publik key
+         */
+        generatePublicKey: function (btn) {
+            var me = this, form = btn.up('formpanel');
+            /**
+             * just for demo generate a key
+             * needs to be generated on the backend using wg genkey
+             */
+            var key = btoa(Math.random().toFixed(32).substr(2));
+            form.getFields('publicKey').setValue(key)
         }
+
     }
 });
