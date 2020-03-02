e8d421adcc1bb9ee5465a1588a5acf0426fbff0e
John Sommerville <jsommerville@untangle.com>
2020-03-02 14:28:12 -0700

    Move validator logic into controller function, add performance enabled check, add licenseInfo into VM once loaded

--- src/package/settings/view/network/interface/Qos.js
+++ src/package/settings/view/network/interface/Qos.js
@@ -9,7 +9,8 @@ Ext.define('Mfw.settings.interface.Qos', {
 
     viewModel: {
         data: {
-            performanceTestEnabled: false
+            performanceTestEnabled: false,
+            licenseInfo: null
         }
     },
 
@@ -54,47 +55,13 @@ Ext.define('Mfw.settings.interface.Qos', {
                     autoComplete: false,
                     decimals: 3,
                     validators: function(val) {
-                        var licInfo = null,
-                         qosAlert = this.up().up().up().down('#qosAlert');
-
-                        console.log("Finding QOS alert:");
-                        console.log(qosAlert);
-
-                        Ext.Ajax.request({
-                            async: false,
-                            url: '/api/status/license',
-                            success: function (response) {
-                                licInfo = Ext.decode(response.responseText);
-                            },
-                            failure: function () {
-                                return true;
-                            }
-                        });
-
-
-                        if(licInfo && licInfo.list.length > 0) {
-                            var licSeats = licInfo.list[0].seats;
-
-                            console.log("Seats:");
-                            console.log(licSeats);
-
-                            console.log("Current val:");
-                            console.log(val);
-
-                            if(!(val >= licSeats - 50 && val <= licSeats + 50)) {
-                                console.log("Display qos alert");
-                                qosAlert.show();
-                            } else {
-                                qosAlert.hide();
-                            }
-                        }
-
-                        return true;
+                        return this.up().up().up().up().up().getController().validateQos();
 
                     }
                 },
                 items: [{
                     label: 'Download Mbps',
+                    itemId: 'qosDl',
                     margin: '0 16 0 0',
                     bind: {
                         value: '{intf._downloadMbps}',
@@ -103,6 +70,7 @@ Ext.define('Mfw.settings.interface.Qos', {
                     }
                 }, {
                     label: 'Upload Mbps',
+                    itemId: 'qosUl',
                     margin: '0 0 0 16',
                     bind: {
                         value: '{intf._uploadMbps}',
@@ -113,7 +81,7 @@ Ext.define('Mfw.settings.interface.Qos', {
             }, {
                 xtype: 'label',
                 itemId: 'qosAlert',
-                html: 'QoS is most effective when configured based on your actual network performance. Use the Test Performance button to find out how your network is performing',
+                html: '',
                 margin: '32 0 0 0',
                 hidden: true
             }, {
@@ -132,11 +100,11 @@ Ext.define('Mfw.settings.interface.Qos', {
         }]
     }],
     listeners: {
-        activate: 'onActivate',
+        initialize: 'onInit',
     },
 
     controller: {
-        onActivate: function (view) {
+        onInit: function (view) {
             var vm = view.getViewModel();
 
             /**
@@ -170,6 +138,65 @@ Ext.define('Mfw.settings.interface.Qos', {
                 xtype: 'performance-dialog',
                 interfaces: [intf]
             }).show();
+        },
+
+        /**
+         * validateQos validates current Qos settings and will display a message if the user has configured QOS outside of the accepted license range.
+         * 
+         * This function is not only a Validator because we also need to access View Model data for the License Info, as well as the PerformanceTestEnabled data.
+         * 
+         */
+
+        validateQos: function() {
+            var me = this,
+                vm = me.getViewModel(),
+                v = me.getView(),
+                licInfo = vm.get('licenseInfo');
+
+
+            // If no License Info exists, call the status/license API to get the license data
+            if(!licInfo) {
+                console.log("Calling license info API...");
+                Ext.Ajax.request({
+                    async: false,
+                    url: '/api/status/license',
+                    success: function (response) {
+                        licInfo = Ext.decode(response.responseText);
+                    },
+                    failure: function () {
+                        return true;
+                    }
+                });
+
+                vm.set('licenseInfo', licInfo)
+            }
+
+            var qosAlert = v.down('#qosAlert'),
+                dlMbps = v.down('#qosDl').getValue(),
+                ulMbps = v.down('#qosUl').getValue(),
+                seats = licInfo.list[0].seats,
+                perfEnabled = vm.get('performanceTestEnabled');
+
+            // if Test Performance is disabled, then we want to display a different button
+            if(perfEnabled) {
+                qosAlert.setHtml('QoS is most effective when configured based on your actual network performance. Use the Test Performance button to find out how your network is performing');
+            } else {
+                qosAlert.setHtml('QoS is most effective when configured based on your actual network performance.');
+            }
+
+            // If seats exists on the license, then we want to toggle the message depending on the QOS settings.
+            if(seats) {
+                var upperLic = seats + 50,
+                    lowerLic = seats - 50;
+
+                if(!(dlMbps >= lowerLic && dlMbps <= upperLic) || !(ulMbps >= lowerLic && ulMbps <= upperLic)) {
+                    qosAlert.show();
+                } else {
+                    qosAlert.hide();
+                }
+            }
+
+            return true;
         }
     }
 });
