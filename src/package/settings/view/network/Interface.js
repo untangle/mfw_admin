4987b1d72f8a435e87984ca0d5f4f4190d9477eb
dhadarau <dhadarau@untangle.com>
2020-03-11 14:44:09 +0200

    Enabled QoS settings for LTE interface
    MFW-806

--- src/package/settings/view/network/Interface.js
+++ src/package/settings/view/network/Interface.js
@@ -46,7 +46,6 @@ Ext.define('Mfw.settings.network.Interface', {
             _hiddenQos: function (get) {
                 return !get('intf.wan') ||
                     get('intf.type') === 'WIFI' ||
-                    get('intf.type') === 'WWAN' ||
                     get('intf.type') === 'OPENVPN' ||
                     get('intf.type') === 'WIREGUARD' ||
                     get('intf.type') === 'VLAN';
