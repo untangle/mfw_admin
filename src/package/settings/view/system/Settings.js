17d908f83fe229c2670c8af8dc607d46306a8cfe
dhadarau <dhadarau@untangle.com>
2020-04-30 15:13:25 +0300

    More consistent Save button placement across views
    MFW-561

--- src/package/settings/view/system/Settings.js
+++ src/package/settings/view/system/Settings.js
@@ -43,7 +43,7 @@ Ext.define('Mfw.settings.system.Settings', {
             options: Map.options.timezones
         }],
         buttons: [{
-            text: 'Save',
+            text: 'Update',
             ui: 'action',
             handler: 'onSave'
         }]
