3683f31564de50543b662eb50a7339f081e084bf
John Sommerville <jsommerville@untangle.com>
2020-09-23 14:25:41 -0600

    update ID typings for boundInterfaceId and add VLAN ID to model fields

--- src/package/settings/model/Interface.js
+++ src/package/settings/model/Interface.js
@@ -88,6 +88,9 @@ Ext.define('Mfw.model.Interface', {
         { name: 'natEgress',  type: 'boolean', allowNull: true },
         { name: 'natIngress', type: 'boolean', allowNull: true },
 
+        //VLAN
+        { name: 'vlanid', type: 'integer', allowNull: true },
+
         // IPv4
         { name: 'v4ConfigType',    type: 'string', allowNull: true }, // ["STATIC","DHCP","PPPOE","DISABLED"]
         // IPv4 STATIC
@@ -160,7 +163,7 @@ Ext.define('Mfw.model.Interface', {
         { name: 'openvpnUsernamePasswordEnabled', type: 'boolean', allowNull: true },
         { name: 'openvpnUsername',                type: 'string',  allowNull: true },
         { name: 'openvpnPasswordBase64',          type: 'string',  allowNull: true },
-        { name: 'boundInterfaceId',        type: 'string',  allowNull: true },
+        { name: 'boundInterfaceId',        type: 'integer',  allowNull: true },
         // ! hasOne openvpnConfFile
 
         // wireguard
